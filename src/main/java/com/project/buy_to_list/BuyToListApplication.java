0ba2eff1f098ecc2ceb0d363c7d0c067fe2bc402
J. Klesley <klesley.pfeitosa@gmail.com>
2025-09-26 20:29:07 -0300

    initial commit

---
+++ src/main/java/com/project/buy_to_list/BuyToListApplication.java
@@ -0,0 +1,13 @@
+package com.project.buy_to_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyToListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyToListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/buy_to_list/BuyToListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.buy_to_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyToListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
